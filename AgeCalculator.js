4dfdba936d9e0a4eed6169fdd030bf413e3d2fb0
Anna B <anna.barnas91@gmail.com>
2018-03-17 20:12:33 +0100

    Simple Age Calculator

---
+++ AgeCalculator.js
@@ -0,0 +1,3 @@
+var	Age = prompt("How old are you?");
+var Days = alert("Congratulations!!You have lived for " + Age * 365.25 + " days.");
+var Hour = alert("That equals " + Age * 365.25 * 24 + " hours.");
